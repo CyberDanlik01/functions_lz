5e8fec9ecd9dd3dd8d665b9bf5d0ad8eb7523fea
CyberDanlik01 <gorillaa@tutamail.com>
2025-11-27 15:38:30 +0300

    Update time.py

--- time.py
+++ time.py
@@ -1,13 +1,17 @@
+#Функция перевода времени в секунды
 def time_in_sec(hs, min, sec):
-    return ((hs*3600) + (min*60) + sec)
+    return ((hs * 3600) + (min * 60) + sec)
 
-#Введем данные с клавиатуры
-h = int(input("Введите часы: "))
-m = int(input("Введите минуты: "))
-s = int(input("Введите секунды: "))
+# Основная функция программы
+def main():
+    h = int(input("Введите часы: "))
+    m = int(input("Введите минуты: "))
+    s = int(input("Введите секунды: "))
+    
+    res = time_in_sec(h, m, s)
+    print("В секундах:", res)
 
-#Переведем в секунды
-res = time_in_sec(h, m, s)
+if __name__ == "__main__":
+    main()
 
-#Результат
-print("В секундах:", res)
+
