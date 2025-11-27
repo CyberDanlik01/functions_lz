e1913a5582550cc48b3f1020fea9ed693c945aea
CyberDanlik01 <gorillaa@tutamail.com>
2025-11-27 15:56:19 +0300

    Update is_even.py

--- is_even.py
+++ is_even.py
@@ -1,19 +1,17 @@
-#Напишем ф-ию для проверки четности числа
 def is_even(number):
-    if number %2 == 0:
+    if number % 2 == 0:
         return True
     else:
         return False
 
 def main():
-#Введем число с клавиатуры
-a = int(input("Введите число: "))
+    # Введем число с клавиатуры
+    a = int(input("Введите число: "))
 
-if is_even(a):
-    print("Четное")
-else:
-    print("Нечетное")
+    if is_even(a):
+        print("Четное")
+    else:
+        print("Нечетное")
 
 if __name__ == "__main__":
     main()
-
