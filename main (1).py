7315233f90a74a4b93a8c23ac8f382bd57282656
CyberDanlik01 <gorillaa@tutamail.com>
2025-11-27 15:49:26 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,13 @@
+import circle
+import temperature
+import is_even
+import time
+
+def main():
+    circle.circle_area()
+    temperature.C_in_F()
+    is_even.is_even()
+    time.time_in_sec()
+
+if __name__ == '__main__':
+    main()
